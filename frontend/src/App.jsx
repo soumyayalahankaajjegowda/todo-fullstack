5e2c14b61644c55f03da765d16bc0186aa6a2036
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-05 09:45:19 -0700

    latestupdate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  async function test() {
+    fetch("http://localhost:8080/test")
+  }
+ return()
+}
+export default App
